e12a3175d486d56762a312ea718b7d9d80c2f95e
it-iandeepak <deepakkumar84142005@gmail.com>
2025-11-04 06:59:20 +0530

    Add day1 solution by Deepak

--- answers/deepak/day1-solution.java
+++ answers/deepak/day1-solution.java
@@ -1,21 +1,25 @@
-public class day1-solution {
+public class Solution {
     static String Spooky(String s )
     {
         StringBuilder result = new StringBuilder();
-        if (s.isEmpty()) return null;
-        for(int i =0; i<s.length(); i++)  // traversing the String
+        if (s.isEmpty()) return "String is empty";
+        String str = s.replaceAll("[^a-z0-9A-Z]","");
+        for(int i =0; i<str.length(); i++)  // traversing the String
         {
-            char ch = s.charAt(i);  // storing each character of string into ch variable
-            if(ch == '_' || ch == '-')  // comparing if it's true we add "~"
-            {
-                result.append('~');
-
-            } else if (i%2==0) {  //  condition for converting uppercase
+            char ch = str.charAt(i);  // storing each character of string into ch variable
+            if (i%2==0) {  //  condition for converting uppercase
                 ch = Character.toUpperCase(ch);
                 result.append(ch);
             } else {             // converting lowercase
                 ch =Character.toLowerCase(ch);
                 result.append(ch);
+            }
+        }
+        for (int i=0; i<s.length(); i++)
+        {
+            if (s.charAt(i) == '_' || s.charAt(i) == '-')
+            {
+                result.insert(i,'~');
             }
         }
         String ans = new String(result);
@@ -28,4 +32,3 @@ public class day1-solution {
         System.out.println(Spooky("var_1-name"));
     }
 }
-// my day 1 solution
