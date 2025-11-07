344d7e7bdd759f0877ac35825ec031909fa7edfd
KUMARI-SONALIUPADHYAY <upadhyaysonali2005@gmail.com>
2025-11-07 22:30:49 +0530

    Added solution for 11-05-2025 - KUMARI SONALI

---
+++ Daily-Solutions/11-05-2025/KUMARISONALIUPADHYAY_6606175.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class KUMARISONALIUPADHYAY_6606175 {
+ public static void main(String[] args){
+ Scanner scan =new Scanner(System.in);
+ System.out.println("enter a sentence:");
+ String sentence = scan.nextLine();
+ int count =countwords(sentence);
+ System.out.println("numbers of words:"+ count);
+ scan.close();
+ }
+
+ //here is countwords function code 
+ private static int countwords(String sentence) {
+    //if this is empty 
+  if(sentence ==null || sentence.isEmpty()){
+    return 0;
+  }
+  int count = 0;
+  boolean insidedata =false;
+  for(int i=0;i<sentence.length();i++){
+    char ch = sentence.charAt(i);
+    //if there is no space
+  if (ch != ' ' && !insidedata) {
+        insidedata = true;
+        count++;
+    }
+    //if there is space
+    else if( ch==' '){
+        insidedata= false;
+    }
+  }
+  return count;
+ }
+    
+}
